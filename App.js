32d523867a98ea8b8096cb7969f8cfb19238dce0
Aman Mittal <amandeepmittal@live.com>
2019-12-12 13:59:01 +0530

    replace reanimated with animated

--- App.js
+++ App.js
@@ -1,24 +1,49 @@
 import React from 'react'
-import { Image, View, Dimensions } from 'react-native'
-import Animated from 'react-native-reanimated'
+import { View, Dimensions, Animated } from 'react-native'
+import { PinchGestureHandler, State } from 'react-native-gesture-handler'
 
 const { width } = Dimensions.get('window')
 
-// import PinchZoom from './PinchZoom'
-
 const App = () => {
+  scale = new Animated.Value(1)
+
+  onZoomEvent = Animated.event(
+    [
+      {
+        nativeEvent: { scale: this.scale }
+      }
+    ],
+    {
+      useNativeDriver: true
+    }
+  )
+
+  onZoomStateChange = event => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      Animated.spring(this.scale, {
+        toValue: 1,
+        useNativeDriver: true
+      }).start()
+    }
+  }
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Animated.Image
-        source={{
-          uri: 'https://miro.medium.com/max/1080/1*7SYuZvH2pZnM0H79V4ttPg.jpeg'
-        }}
-        style={{
-          width: width,
-          height: 300
-        }}
-        resizeMode='contain'
-      />
+      <PinchGestureHandler
+        onGestureEvent={this.onZoomEvent}
+        onHandlerStateChange={this.onZoomStateChange}>
+        <Animated.Image
+          source={{
+            uri:
+              'https://miro.medium.com/max/1080/1*7SYuZvH2pZnM0H79V4ttPg.jpeg'
+          }}
+          style={{
+            width: width,
+            height: 300,
+            transform: [{ scale: this.scale }]
+          }}
+          resizeMode='contain'
+        />
+      </PinchGestureHandler>
     </View>
   )
 }
